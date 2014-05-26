483327ed7bc15fa44aa4699de74b164d5d835fba
Louis T <LouisT@ltdev.im>
2014-05-26 16:49:45 -0400

    Fix font paths and edit index.php to display some meta data as well as charset

--- web/index.php
+++ web/index.php
@@ -1,6 +1,11 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" itemscope itemtype="http://schema.org/CreativeWork">
 <head>
 <title>Simple Server Status</title>
+<meta itemprop="name" content="Simple Server Status">
+<meta itemprop="description" content="Simple multi-server live status monitoring.">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/reset.css">
 <link rel="stylesheet" href="css/icons.css">
 <!--[if IE 7]>
